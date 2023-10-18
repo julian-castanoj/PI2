1e59996297f55cce15ae9fc323434e881031b951
Julian Castaño <julian.castanoj@udea.edu.co>
2023-10-18 05:18:31 -0500

    navbar

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Sidebar from './components/Sidebar';
+import Navbar from './components/Navbar';
 import Inicio from './pages/Inicio.jsx';
 import Usuarios from './pages/Usuarios.jsx';
 import Estadisticas from './pages/Estadisticas.jsx';
@@ -19,29 +20,35 @@ import RegistrarTransacciones from './pages/RegistrarTransacciones.jsx'
 const App = () => {
   return (
     <BrowserRouter>
-      <Sidebar>
-        <Routes>
-          <Route path="/" element={<Inicio />} />
-          <Route path="/inicio" element={<Inicio />} />
+      <div className="app-container">
+        <Navbar />
+        <div className="content-container"></div>
+        <Sidebar>
+          <Routes>
+
+            <Route path="/" element={<Inicio />} />
+            <Route path="/inicio" element={<Inicio />} />
 
 
-          <Route exact path="/usuarios" element={<Usuarios />} />
-          <Route exact path="/registrarMiembros" element={<RegistrarMiembros />} />
+            <Route exact path="/usuarios" element={<Usuarios />} />
+            <Route exact path="/registrarMiembros" element={<RegistrarMiembros />} />
 
-          <Route exact path="/gestores" element={<Gestores />} />
-          <Route exact path="/registrarGestores" element={<RegistrarGestores />} />
+            <Route exact path="/gestores" element={<Gestores />} />
+            <Route exact path="/registrarGestores" element={<RegistrarGestores />} />
 
-          <Route exact path="/transformadores" element={<Transformadores />} />
-          <Route exact path="/registrarTransformadores" element={<RegistrarTransformadores />} />
+            <Route exact path="/transformadores" element={<Transformadores />} />
+            <Route exact path="/registrarTransformadores" element={<RegistrarTransformadores />} />
 
-          <Route path="/transacciones" element={<Transacciones />} />
-          <Route path="/registrarTransacciones" element={<RegistrarTransacciones />} />
+            <Route path="/transacciones" element={<Transacciones />} />
+            <Route path="/registrarTransacciones" element={<RegistrarTransacciones />} />
 
-          <Route path="/estadisticas" element={<Estadisticas />} />
-          <Route path="/logout" element={<Logout />} />
-        </Routes>
-      </Sidebar>
-    </BrowserRouter>
+            <Route path="/estadisticas" element={<Estadisticas />} />
+            <Route path="/logout" element={<Logout />} />
+          </Routes>
+        </Sidebar>
+      </div>
+    
+    </BrowserRouter >
   );
 };
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/Navbar.css';
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-content">
+        
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
